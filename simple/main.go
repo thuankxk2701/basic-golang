e60822d8f679346ef95f9352e7cc8ef095cd91c4
Nguyen Van Thuan <ttctde@gmail.com>
2023-07-04 16:00:57 +0700

    Initial basic golang

---
+++ simple/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	cards :=[]string {"Get","Post" }
+             
+        cards = append(cards,"Put");
+        
+        for i, card :=range cards{
+        fmt.Println(i,card)
+          }
+}
+
+
+func newCard() string{
+
+	return "Five of diamonds"
+}
+
+
+
