b5e43f2618cf15fd7aa46d6c7b103200c9edded1
Nguyen Van Thuan <ttctde@gmail.com>
2023-07-05 11:34:43 +0700

    hexadecimal

---
+++ simple/numeral-systems/hexadecimal/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	//fmt.Printf("%d - %b - %x \n", 42, 42, 42)    //42 - 101010 - 2a
+	//fmt.Printf("%d - %b - %#x \n", 42, 42, 42)   //42 - 101010 - 0x2a
+	//fmt.Printf("%d - %b - %#X \n", 42, 42, 42)   //42 - 101010 - 0X2A
+	//fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42) //42       101010          0X2A
+}
