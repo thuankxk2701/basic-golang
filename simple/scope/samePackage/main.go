48eb97e8aa2a79c663941de3467eea7ec06a8cde
Nguyen Van Thuan <ttctde@gmail.com>
2023-07-06 09:53:29 +0700

    same package

---
+++ simple/scope/samePackage/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(x) // Get variable x in the file same.go and return  result 7
+
+}
+
+// IMPORTANT
+// to run this code:
+// go run *.go
+// ---- OR ----
+// go build
