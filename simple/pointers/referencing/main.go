40bd37f4d571d11d77c0ed774c03ba82304c3fb0
Nguyen Van Thuan <ttctde@gmail.com>
2023-07-06 11:36:06 +0700

    dereferencing

--- simple/pointers/referencing/main.go
+++ simple/pointers/referencing/main.go
@@ -17,4 +17,10 @@ func main() {
 	// The above code makes b a pointer to the memory address where an int is stores
 	// b is of type "int pointer"
 	// *int -- the * is part of the type -- b is of type *int
+
+	// b is an int pointer;
+	// b points to the memory address where an int is stored
+	// to see the value in that memory address, add a * in front of b
+	// this is known as deReferencing
+	// the * is an operator in this case
 }
