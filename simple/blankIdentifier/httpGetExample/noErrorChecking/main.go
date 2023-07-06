9477ef74fa845fd723677e766c8e4a9778be24d7
Nguyen Van Thuan <ttctde@gmail.com>
2023-07-06 10:11:13 +0700

    handle http get

---
+++ simple/blankIdentifier/httpGetExample/noErrorChecking/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	res, _ := http.Get("http://www.geekwiseacademy.com/")
+	page, _ := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	fmt.Printf("page -%s \n", page)
+	fmt.Printf("res - %v \n", res)
+}
