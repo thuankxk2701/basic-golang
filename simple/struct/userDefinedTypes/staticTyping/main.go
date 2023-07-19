982f4d0ba36be0769a73c672268a501d1561d5ed
Nguyen Van Thuan <thuan.nv@evotek.vn>
2023-07-19 16:46:20 +0700

    method and embedded

--- simple/struct/userDefinedTypes/staticTyping/main.go
+++ simple/struct/userDefinedTypes/staticTyping/main.go
@@ -12,4 +12,10 @@ func main() {
 	var yourAge int
 	yourAge = 29
 	fmt.Printf("%T %v \n", yourAge, yourAge)
+
+	// this doesn't work:
+	//	 fmt.Println(myAge + yourAge)
+
+	// this conversion works:
+	//	 fmt.Println(int(myAge) + yourAge)
 }
