0a74394420126a66b190fff3ad8c4cc92daab2e5
Nguyen Van Thuan <thuan.nv@evotek.vn>
2023-07-19 16:14:15 +0700

    use defined type

---
+++ simple/struct/userDefinedTypes/staticTyping/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type foo int
+
+func main() {
+	var myAge foo
+	myAge = 44
+	fmt.Printf("%T %v \n", myAge, myAge)
+
+	var yourAge int
+	yourAge = 29
+	fmt.Printf("%T %v \n", yourAge, yourAge)
+}
