f59bdbe05245f5976a1c788c29541dc833249920
Nguyen Van Thuan <ttctde@gmail.com>
2023-07-30 19:43:46 +0700

    UnMarshal

--- simple/struct/marshalUnmarshal/marshal/tag/main.go
+++ simple/struct/marshalUnmarshal/marshal/tag/main.go
@@ -11,8 +11,27 @@ type person struct {
 	Age   int    `json:"Wisdom score"`
 }
 
+type personUnMarshal struct {
+	First string
+	Last  string
+	Age   int `json:"wisdom score"`
+}
+
 func main() {
 	p1 := person{"James", "Bond", 20}
 	bs, _ := json.Marshal(p1)
 	fmt.Println(string(bs))
+
+	var pUn1 personUnMarshal
+	fmt.Println(pUn1.First)
+	fmt.Println(pUn1.Last)
+	fmt.Println(pUn1.Age)
+
+	bsUn := []byte(`{"First":"James", "Last":"Bond", "Age":20}`)
+	json.Unmarshal(bsUn, &pUn1)
+	fmt.Println("--------------")
+	fmt.Println(pUn1.First)
+	fmt.Println(pUn1.Last)
+	fmt.Println(pUn1.Age)
+	fmt.Printf("%T \n", p1)
 }
